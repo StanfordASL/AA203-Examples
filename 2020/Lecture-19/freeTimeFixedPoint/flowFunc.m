59e77c7b7279ef955692f1ad10e193f3c6fda657
Ed Schmerling <ednerd@gmail.com>
2021-03-30 19:55:11 -0700

    Moving ./2019/ to a '2019' branch

--- 2020/Lecture-19/freeTimeFixedPoint/flowFunc.m
+++
@@ -1,8 +0,0 @@
-function [fl,flDer] = flowFunc(y) % Parabolic flow in 0 \le y \le l
-
-global l;
-
-coeff = 0.35; % Leading coefficient: maximal magnitude
-normFlow = l^2/4.0; % To impose: \int^l_0 flow(y(s)) ds = coeff
-fl = (coeff/normFlow)*y*(l - y);
-flDer = (coeff/normFlow)*(l - 2*y);
